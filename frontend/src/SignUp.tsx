95df986a2ce37378fcdf5f772549b21756fee672
as-test <ashraf.alghamdi.ruh.js@tuwaiq.edu.sa>
2021-11-15 12:03:48 +0300

    Add SignUp component

---
+++ frontend/src/SignUp.tsx
@@ -0,0 +1,24 @@
+function SignUp(){
+    return (
+        <div className="container mt-5" style={{width: "500px"}}>
+            <div className="card shadow">
+                <div className="card-body">
+                    <h4 className="text-center mb-4">Sign-up</h4>
+                    <form method="post">
+                    <label htmlFor="usrName" className="form-label">Username</label>
+                    <input type="text" name="usrName" className="form-control"/>
+                    <label htmlFor="usrPass" className="form-label mt-3">Password</label>
+                    <input type="password" name="usrPass" className="form-control"/>
+                    <label htmlFor="reUsrPass" className="form-label mt-3">Re-enter password</label>
+                    <input type="password" name="reUsrPass" className="form-control"/>
+                    <div className="d-flex justify-content-end">
+                        <input type="submit" value="Sign-up" className="btn btn-dark mt-3"/>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export {SignUp}
