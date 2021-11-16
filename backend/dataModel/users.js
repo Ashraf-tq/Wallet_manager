fbc6ea98b0a927b715e9a1e7dc0bfc1026b5bb3a
Amjad <amjad@Amjads-MacBook-Air.local>
2021-11-16 10:00:15 +0300

    Added user data model

---
+++ backend/dataModel/users.js
@@ -0,0 +1,11 @@
+
+
+const user = (name,email,password)=>{
+    const model ={
+        "username":name,
+        "useremail":email,
+        "password":password,
+    }
+}
+
+export{user}
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const users = require('./data/users.json')
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('OK!')
 })
 
+app.get('/api/users',(req,res)=>{
+res.send(users)
+})
+
+app.put('/api/users/put',(req,res)=>{
+  res.send("put")
+  
+})
   
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
